ba2597bd5af33c1ad9c62cd42747a4d71d9eb21f
严浩 <1424412158@qq.com>
2020-03-20 16:00:55 +0800

    3

--- W43/W43.cpp
+++ W43/W43.cpp
@@ -1,20 +1,47 @@
-﻿// W43.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿//
+//  main.cpp
+//  test
+//
+//  Created by 徐亦燊 on 2020/2/28.
+//  Copyright  2020 徐亦燊. All rights reserved.
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat binaryMat;
+	cv::Mat labelMat;
+	cv::Mat statsMat;
+	cv::Mat centrMat;
+	cv::Mat srcMat = imread("C://project//pic2.jpg", 0);
+	bitwise_not(srcMat, srcMat);   
+	cv::threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+	cv::Mat kernel;
+	kernel = getStructuringElement(MORPH_ELLIPSE, Size(9, 11));
+	cv::morphologyEx(binaryMat, binaryMat, cv::MORPH_OPEN, kernel);
+	int nComp = cv::connectedComponentsWithStats(binaryMat, labelMat, statsMat, centrMat, 8, CV_32S);
+	for (int i = 0; i < nComp; i++)
+	{
+		cout << "connected Components NO. " << i << endl;
+		cout << "pixels = " << statsMat.at<int>(i, 4) << endl;
+		cout << "width = " << statsMat.at<int>(i, 2) << endl;
+		cout << "height = " << statsMat.at<int>(i, 3) << endl;
+		cout << endl;
+	}
+	for (int i = 1; i < nComp; i++)
+	{
+		Rect bndbox;
+		bndbox.x = statsMat.at<int>(i, 0);
+		bndbox.y = statsMat.at<int>(i, 1);
+		bndbox.width = statsMat.at<int>(i, 2);
+		bndbox.height = statsMat.at<int>(i, 3);
+		rectangle(binaryMat, bndbox, CV_RGB(255, 255, 255), 1, 8, 0);
+	}
+	imshow("binaryMat", binaryMat);
+	waitKey(0);
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
